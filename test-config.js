41a7985fc21eb3c3d39f20d575c1c6d7672638ad
amitk09 <toamitkumar09@gmail.com>
2021-09-21 17:51:41 +0530

    adding serve in report to open report immediatly after execution

--- test-config.js
+++ test-config.js
@@ -36,11 +36,11 @@ exports.config ={
 
     onComplete: function() {
         const reportError = new Error('Could not generate Allure report')
-        const generation = allure(['generate', 'allure-results', '--clean'])
+        const generation = allure(['serve', 'allure-results', '--clean'])
         return new Promise((resolve, reject) => {
             const generationTimeout = setTimeout(
                 () => reject(reportError),
-                5000)
+                10000)
 
             generation.on('exit', function(exitCode) {
                 clearTimeout(generationTimeout)
